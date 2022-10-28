832d223ec5da19c182b3c4597faf5b337798cf95
Thomas GRUEL <gruelt@gmail.com>
2022-10-28 22:30:53 +0200

    initial import

---
+++ layouts/public.vue
@@ -0,0 +1,18 @@
+  <template>
+    <div>
+      This is my publicaaaa
+
+      <Nuxt />
+    </div>
+  </template>
+
+
+<script>
+export default {
+  name: "public.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    about
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "about.vue",
+  layout: "public"
+}
+</script>
+
+<style scoped>
+
+</style>
